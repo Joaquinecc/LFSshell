5496c6253c65fce12dc70649a3a6eee5321d4a13
joaquinecc <joaquinecc@gmail.com>
2020-12-05 16:45:08 -0300

    command rename

--- LFSshell.py
+++ LFSshell.py
@@ -1,4 +1,5 @@
 import shutil
+import os
 def get_paths(paths):
         #Obtenemos los path de los parametros
     try:
@@ -12,6 +13,42 @@ def get_paths(paths):
     except:
         print("Invalid parameter")
         return False
+def shell_rename(args):
+    #Obtenemos los paths
+    paths=get_paths(args)
+    if paths == False :
+        return False
+    src=paths[0] 
+    new_name=paths[1]
+    #Verificamos si el new name no es una direccion
+    if(new_name.find("/") != -1):
+        print("Second parameter must be a name not an address")
+        print("If you want to move a file and rename it, use command : copy ")
+        return
+
+
+    #Creamos el nuevo path
+    new_path=""
+    index=src.rfind("/")
+    if index == -1:
+        new_path=new_name
+    else:
+        new_path=src.replace(src[index+1:],new_name)
+
+    try:
+        os.rename(src,new_path)
+        print("Rename {0} --> {1}".format(src,new_path))
+    # except FileNotFoundError:
+    #     print('No such file: {0}'.format(src))
+    except IsADirectoryError:
+        print ("new name = {0} is a directory".format(new_name))
+    except NotADirectoryError:
+        print ("src = {0} is a directory".format(src))
+    except OSError as error:
+        print(error)
+    except:
+        print("Invalid parameter")
+
 def shell_move(args):
     #Funcion que simula el movimiento de archivos a directorios
     #Obtenemos los paths
@@ -60,6 +97,8 @@ def main():
             shell_copy(command[5:])
         elif command[:4] == "move":
             shell_move(command[5:])
+        elif command[:6] == "rename":
+            shell_rename(command[7:])
         else:
             print("Command not found")
 
