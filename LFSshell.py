4bd537fd9136a17789dd0d6511e3a545c6722181
joaquinecc <joaquinecc@gmail.com>
2020-12-05 14:25:12 -0300

    set up shell

---
+++ LFSshell.py
@@ -0,0 +1,12 @@
+def main():
+    while True:
+        print("Shell start")
+        coomand = input("shell-$ ")
+        if command == "exit":
+            break
+        else:
+            print("EL comando no se encuentra")
+
+
+if '__main__' == __name__:
+    main()
